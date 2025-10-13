39d6f79e21008e39e787ce0c2a92dc70f7e2e761
strwbrry1 <matvienkosemyon@gmail.com>
2025-10-13 22:14:14 +0400

    added cards

--- lib/main.dart
+++ lib/main.dart
@@ -13,39 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.accents.first),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Test text'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,69 +30,145 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text("Матвиенко С.Д. ПИбд-33"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: const MyWidget(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final data = [
+      _CardData(
+          "text",
+          description: "hello",
+          icon: Icons.skateboarding_outlined,
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Manul_Timofey_in_April_2025_%281%2C_cropped%29.jpg/330px-Manul_Timofey_in_April_2025_%281%2C_cropped%29.jpg'),
+      _CardData(
+          "test",
+          description: "world",
+          icon: Icons.wifi,
+          imageUrl: 'https://static.mk.ru/upload/entities/2024/09/27/06/articles/facebookPicture/1d/39/cc/b7/8e98fb7312647c5aaf958cae41c94691.jpg',
+      ),
+      _CardData(
+          "something",
+          description: "send help",
+          icon: Icons.sos,
+          imageUrl: 'https://cs9.pikabu.ru/post_img/2017/08/27/4/og_og_1503809823210718041.jpg',
+      ),
+    ];
+
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children:
+            data.map((e) => _Card.fromData(e)).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class _CardData {
+  final String text;
+  final String description;
+  final IconData icon;
+  final String? imageUrl;
+
+  _CardData(
+      this.text, {
+        required this.description,
+        this.icon = Icons.abc,
+        this.imageUrl,
+      });
+}
+
+class _Card extends StatelessWidget {
+  final String text;
+  final String description;
+  final IconData icon;
+  final String? imageUrl;
+
+  const _Card(
+      this.text, {
+        this.icon = Icons.add_alarm,
+        required this.description,
+        this.imageUrl,
+      });
+
+  factory _Card.fromData(_CardData cardData) => _Card(
+    cardData.text,
+    description: cardData.description,
+    icon: cardData.icon,
+    imageUrl: cardData.imageUrl,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 12),
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: Colors.primaries.first.shade100,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: SizedBox(
+                  width: 100,
+                  height: 150,
+                  child: Image.network(
+                    imageUrl ?? '',
+                    fit: BoxFit.cover,
+                    errorBuilder: (_, __, ___) => const Placeholder(),
+                  ),
+                ),
+              )
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    this.text,
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  Text(
+                    this.description,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: Icon(icon),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
